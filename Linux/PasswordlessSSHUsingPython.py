3ca8d01b2dd56348c7e2e50fb4ff904066d0c18d
Waqas Ali <lucifer501@yahoo.com>
2018-08-24 10:29:24 -0400

    Add files via upload

---
+++ Linux/PasswordlessSSHUsingPython.py
@@ -0,0 +1,15 @@
+def connect_ssh():
+    sshProcess = subprocess.Popen(['ssh', 
+                   'vadmin@192.168.1.1'],
+                   stdin=subprocess.PIPE, 
+                   stdout = subprocess.PIPE,
+                   universal_newlines=True,
+                   bufsize=0)
+    return sshProcess
+
+
+sshProcess = connect_ssh()
+sshProcess.stdin.write("your command here.")
+sshProcess.stdin.close()
+for line in sshProcess.stdout:
+    print(line)
